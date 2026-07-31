234500c51de0069f12b4e6d3f7cfe9f28a54728c
Mourede2026 <sossouhounto_2000@hotmail.com>
2026-07-31 04:53:30 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-/* Service worker désactivé côté index.html (voir la balise <script> en bas
-   de index.html qui appelle unregister()). Ce fichier est conservé pour
-   référence mais n'est plus enregistré activement pour le moment. */
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
