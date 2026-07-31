110a3afe700be74c10972938c2f5ab1116be0b23
Mourede2026 <sossouhounto_2000@hotmail.com>
2026-07-31 05:06:06 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+// ATTENTION : remplace cette valeur par l'URL de ton Web App Apps Script
+// une fois déployée (voir README.md, étape 3).
+// Elle ressemble à : https://script.google.com/macros/s/AKfycb.../exec
+const API_URL = 'COLLE_ICI_TON_URL_APPS_SCRIPT';
+
+const MOIS_LABELS = [
+  'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+  'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
+];
+
+function formaterMois(moisAAAAMM) {
+  if (!moisAAAAMM) return '';
+  const [annee, mois] = moisAAAAMM.split('-');
+  return MOIS_LABELS[parseInt(mois, 10) - 1] + ' ' + annee;
+}
+
+function moisActuel() {
+  const d = new Date();
+  return d.getFullYear() + '-' + String(d.getMonth() + 1).padStart(2, '0');
+}
